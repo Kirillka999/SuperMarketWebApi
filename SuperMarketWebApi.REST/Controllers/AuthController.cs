6afe0d9d54e7f1e3449a0d1f4dcfd29cd8286ed8
Kirill <kirill93549@gmail.com>
2025-07-11 08:58:39 +0300

    Added ProductController

--- SuperMarketWebApi.REST/Controllers/AuthController.cs
+++ SuperMarketWebApi.REST/Controllers/AuthController.cs
@@ -1,10 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 
+public record SignUpRequest(string Email, string FullName, string Password);
+
+public record SignInRequest(string Email, string Password);
+
+public record SignInResponse(string AccessToken);
+
 namespace SuperMarketWebApi.Controllers
 {
-    
+    [ApiController]
+    [Route("api/v1/[controller]/[action]")]
     public class AuthController : Controller
     {
+        
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInRequest request, CancellationToken ct)
+        { 
+            return Ok();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpRequest request, CancellationToken ct)
+        {
+            return Ok();
+        }
     
     }
 }
--- SuperMarketWebApi.REST/Controllers/ProductController.cs
+++ SuperMarketWebApi.REST/Controllers/ProductController.cs
@@ -1,6 +1,43 @@
-namespace SuperMarketWebApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
 
-public class ProductController
+public record NewProductRequest(string Email, string FullName, string Password);
+public record UpdateProductRequest(string Email, string FullName, string Password);
+
+namespace SuperMarketWebApi.Controllers
 {
-    
+    [ApiController]
+    [Route("api/v1/products")]
+    public class ProductController : Controller
+    {
+        
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts(CancellationToken ct)
+        { 
+            return Ok();
+        }
+        
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetProductById(int id, CancellationToken ct)
+        {
+            return Ok();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> CreateNewProduct(NewProductRequest product, CancellationToken ct)
+        { 
+            return Ok();
+        }
+        
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> UpdateProductById(int id, UpdateProductRequest product, CancellationToken ct)
+        { 
+            return Ok();
+        }
+        
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteProductById(int id, CancellationToken ct)
+        { 
+            return Ok();
+        }
+    }
 }
