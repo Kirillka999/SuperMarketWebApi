d4f0f5cdf33b61019d78f7c331fca41fdec6b7b6
Kirill <kirill93549@gmail.com>
2025-07-14 14:06:58 +0300

    added migration

---
+++ SuperMarketWebApi.Persistence/Migrations/20250714075523_AddedCartCreatedAtField.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SuperMarketWebApi.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCartCreatedAtField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Cart",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Cart");
+        }
+    }
+}
